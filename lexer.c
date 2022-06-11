607e198f9823212944627089209484e5016e123d
Oussama Lakhdar <lakhdaroussama99@gmail.com>
2022-06-11 15:01:38 +0100

    step1

--- lexer.c
+++ lexer.c
@@ -6,7 +6,7 @@
 /*   By: olakhdar <olakhdar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 21:37:16 by olakhdar          #+#    #+#             */
-/*   Updated: 2022/06/10 22:23:24 by olakhdar         ###   ########.fr       */
+/*   Updated: 2022/06/11 12:22:09 by olakhdar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ char	*putspace(char *s)
 	i = 0;
 	cot = 0;
 	str = ft_strdup("");
+	s = remove_space(s);
 	while (s[i])
 	{
 		if (s[i + 1] == '|' || s[i + 1] == '<' || s[i + 1] == '>')
@@ -29,7 +30,6 @@ char	*putspace(char *s)
 			{    
 				str = charjoin(str, s[i]);
 				str = charjoin(str, ' ');
-				//i++;
 			}
 			else
 				str = charjoin(str, s[i]);
@@ -40,7 +40,6 @@ char	*putspace(char *s)
 			{
 				str = charjoin(str, s[i]);
 				str = charjoin(str, ' ');
-				//i++;
 			}
 			else
 				str = charjoin(str, s[i]);
@@ -54,9 +53,35 @@ char	*putspace(char *s)
 
 char	*remove_space(char *s)
 {
+	int		i;
 	char	*str;
 
-	
-			
+	i = 0;
+	str = ft_strdup("");
+	while(s[i])	
+	{
+		if (s[i] == '\"')
+		{
+			str = charjoin(str, s[i++]);
+			while(s[i] != '\"')
+			{
+				if (s[i] == ' ')
+					str = charjoin(str, '*');
+				else if (s[i] == '|')
+					str = charjoin(str, '+');
+				else if (s[i] == '>')
+					str = charjoin(str, '=');
+				else if (s[i] == '<')
+					str = charjoin(str, '^');
+				else
+					str = charjoin(str, s[i]);
+				i++;
+			}
+			str = charjoin(str, s[i]);
+		}
+		else
+			str = charjoin(str, s[i]);
+		i++;
+	}
 	return (str);
 }
